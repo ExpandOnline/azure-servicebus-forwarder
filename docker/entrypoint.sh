478351f378a28c4855d2585b9472ebfb16262261
pvaass <pvaassens@expandonline.nl>
2019-04-12 11:14:01 +0200

    S150.11 Correct supervisord

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-export AWS_DEFAULT_REGION=eu-central-1
-aws s3 cp s3://${BUCKET_NAME}/config.yml /app/config.yml
-aws s3 cp s3://${BUCKET_NAME}/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+if [[ -v BUCKET_NAME ]]; then
+  export AWS_DEFAULT_REGION=eu-central-1
+  aws s3 cp s3://${BUCKET_NAME}/config.yml /app/config.yml
+  aws s3 cp s3://${BUCKET_NAME}/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+fi
 
-/usr/bin/supervisord
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
